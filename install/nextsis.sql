6b4d5b8f0c34a36c77517ccb96fdac237256f90f
Mike Moriarty <code@mikemoriarty.com>
2012-10-15 17:11:56 +0900

    New tables and relationships (phpMyAdmin Designer)
    Changed to mysqldump for DROP and CREATE DATABASE functionality.

--- install/nextsis.sql
+++ install/nextsis.sql
@@ -1,79 +1,239 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.24, for debian-linux-gnu (i686)
 --
--- Host: localhost
--- Generation Time: Oct 15, 2012 at 12:10 AM
--- Server version: 5.5.24
--- PHP Version: 5.3.10-1ubuntu3.4
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: nextsis
+-- ------------------------------------------------------
+-- Server version	5.5.24-0ubuntu0.12.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `nextsis`
+-- Current Database: `nextsis`
 --
 
--- --------------------------------------------------------
+/*!40000 DROP DATABASE IF EXISTS `nextsis`*/;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `nextsis` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+
+USE `nextsis`;
+
+--
+-- Table structure for table `course`
+--
+
+DROP TABLE IF EXISTS `course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `course` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Primary key.',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Stores course names and details.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `course`
+--
+
+LOCK TABLES `course` WRITE;
+/*!40000 ALTER TABLE `course` DISABLE KEYS */;
+/*!40000 ALTER TABLE `course` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `person`
 --
 
-CREATE TABLE IF NOT EXISTS `person` (
+DROP TABLE IF EXISTS `person`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `person` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Primary key (4,294,967,295 possible values). This can serve as a public or internal identifier.',
   `local_id` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `surname` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'The surname of the person.',
   `first_name` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'This stores the first name of the person.',
   `middle_name` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'This is to store a person''s middle name.',
   `common_name` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'The name the person is commonly known by.',
+  `title_id` tinyint(3) unsigned NOT NULL COMMENT 'Foreign key from person_title table.',
+  `gender_id` tinyint(3) unsigned NOT NULL COMMENT 'Foreign key from the person_gender table.',
   PRIMARY KEY (`id`),
   UNIQUE KEY `local_id` (`local_id`),
   KEY `surname` (`surname`),
   KEY `first_name` (`first_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Person table (stores all people - students, staff, parents, administrators...)' AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Person table (stores all people - students, staff, parents, administrators...)';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `person`
+--
+
+LOCK TABLES `person` WRITE;
+/*!40000 ALTER TABLE `person` DISABLE KEYS */;
+/*!40000 ALTER TABLE `person` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `person_course`
+--
+
+DROP TABLE IF EXISTS `person_course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `person_course` (
+  `person_id` int(8) unsigned NOT NULL COMMENT 'Foreign key from the person table.',
+  `course_id` mediumint(8) unsigned NOT NULL COMMENT 'Foreign key form the course table.',
+  PRIMARY KEY (`person_id`,`course_id`),
+  KEY `course_id` (`course_id`),
+  CONSTRAINT `person_course_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `person_course_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `person` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Link table between person and course for many-to-many.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `person_course`
+--
+
+LOCK TABLES `person_course` WRITE;
+/*!40000 ALTER TABLE `person_course` DISABLE KEYS */;
+/*!40000 ALTER TABLE `person_course` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `person_role`
+--
+
+DROP TABLE IF EXISTS `person_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `person_role` (
+  `person_id` int(10) unsigned NOT NULL COMMENT 'Foreign key from the person table.',
+  `role_id` smallint(8) unsigned NOT NULL COMMENT 'Foreign key from the role table.',
+  PRIMARY KEY (`person_id`,`role_id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `person_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `person_role_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `person` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Link table between person and role to support many-to-many.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `person_role`
+--
+
+LOCK TABLES `person_role` WRITE;
+/*!40000 ALTER TABLE `person_role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `person_role` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `person_school`
 --
 
-CREATE TABLE IF NOT EXISTS `person_school` (
+DROP TABLE IF EXISTS `person_school`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `person_school` (
   `person_id` int(10) unsigned NOT NULL COMMENT 'Foreign key from person table',
   `school_id` mediumint(8) unsigned NOT NULL COMMENT 'Foriegn key from school table',
   PRIMARY KEY (`person_id`,`school_id`),
-  KEY `school_id` (`school_id`)
+  KEY `school_id` (`school_id`),
+  CONSTRAINT `person_school_ibfk_2` FOREIGN KEY (`person_id`) REFERENCES `person` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `person_school_ibfk_3` FOREIGN KEY (`school_id`) REFERENCES `school` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Link table between person and school for many-to-many';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `person_school`
+--
+
+LOCK TABLES `person_school` WRITE;
+/*!40000 ALTER TABLE `person_school` DISABLE KEYS */;
+/*!40000 ALTER TABLE `person_school` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Primary key.',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Stores people''s roles (student, teacher, parent, administrator etc.)';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `school`
 --
 
-CREATE TABLE IF NOT EXISTS `school` (
+DROP TABLE IF EXISTS `school`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `school` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Stores school details (to support multiple schools)' AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Stores school details (to support multiple schools)';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Constraints for dumped tables
+-- Dumping data for table `school`
 --
 
---
--- Constraints for table `person_school`
---
-ALTER TABLE `person_school`
-  ADD CONSTRAINT `person_school_ibfk_3` FOREIGN KEY (`school_id`) REFERENCES `school` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `person_school_ibfk_2` FOREIGN KEY (`person_id`) REFERENCES `person` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+LOCK TABLES `school` WRITE;
+/*!40000 ALTER TABLE `school` DISABLE KEYS */;
+/*!40000 ALTER TABLE `school` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `school_course`
+--
+
+DROP TABLE IF EXISTS `school_course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `school_course` (
+  `school_id` mediumint(10) unsigned NOT NULL COMMENT 'Foreign key from the school table.',
+  `course_id` mediumint(10) unsigned NOT NULL COMMENT 'Foreign key from the course table.',
+  PRIMARY KEY (`school_id`,`course_id`),
+  KEY `course_id` (`course_id`),
+  CONSTRAINT `school_course_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `school_course_ibfk_1` FOREIGN KEY (`school_id`) REFERENCES `school` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='This is a link table between school and course to support many-to-many.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `school_course`
+--
+
+LOCK TABLES `school_course` WRITE;
+/*!40000 ALTER TABLE `school_course` DISABLE KEYS */;
+/*!40000 ALTER TABLE `school_course` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-10-15 16:50:36
